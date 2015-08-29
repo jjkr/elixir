8ff399ac2e671de3bf741ca111cf5f75391fdf92
Joe Kramer <joe.kramer.cu@gmail.com>
2015-08-29 14:14:50 -0600

    Add phoenix project

--- ping_pong.exs
+++ ping_pong.exs
@@ -1,12 +1,27 @@
-server = spawn fn ->
-  receive do
-    {client, :ping} ->
-      IO.puts "got ping!"
-      send client, :pong
-  end
-end
+#defmodule PingServer
+#  def start do: spawn listen
+#  defp listen do
+#    receive do
+#      {client, :ping} -> do
+#        IO.puts "got ping!"
+#        send client, :pong
+#        listen
+#      end
+#    end
+#  end
+#end
 
+server = PingServer.start
 server |> send {self, :ping}
-receive do
-  _ -> IO.puts "got pong!"
+
+defmodule Client
+  def start do: spawn poll
+  def poll
+    receive do
+      _ -> do
+        IO.puts "got pong!"
+        poll
+      end
+    end
+  end
 end
